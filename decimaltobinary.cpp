a81ff9575e0ce3a307155c2d8692631131f5c20a
Priyambada Mohapatra <priyambadamohapatra29@gmail.com>
2024-01-19 23:06:20 +0530

    info
    added comment for guidence

--- decimaltobinary.cpp
+++ decimaltobinary.cpp
@@ -1,22 +1,18 @@
 #include<iostream>
 #include<math.h>
 using namespace std;
-
-
 int main() {
-
+    //we take a decimal as input
     int n;
     cin >> n;
-
-
     float ans  = 0;
     int i = 0;
     while(n != 0 ) {
 
         int bit  = n & 1;
         cout<<bit<<endl;
-        /*ans= ans*10+bit;*/
-        ans=(bit*pow(10,i))+ans;
+        /*ans= ans*10+bit;*/ // this can also be used 
+        ans=(bit*pow(10,i))+ans;//this is the formula
         n = n >> 1;
         i++;
     }
