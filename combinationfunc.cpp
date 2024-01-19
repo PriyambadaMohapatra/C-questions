d6a812c64191357466a3b7893e58c9339ce54390
Priyambada Mohapatra <priyambadamohapatra29@gmail.com>
2024-01-19 23:04:39 +0530

    info
    added the comments for easy guide

--- combinationfunc.cpp
+++ combinationfunc.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+//funtion for factorial
 int fact(int n){
     int fact=1;
     for(int i=1;i<=n;i++){
@@ -7,14 +8,17 @@ int fact(int n){
     }
     return fact;
 }
+//funtion for ncr(combination)
 int combination(int n, int r){
     int ans= fact(n)/(fact(r)*fact(n-r));
     return ans;
 }
+//the main function
 int main(){
     int n ,r;
     cout<<"ENTER THE VALUE OF n and r:"<<endl;
     cin>>n>>r;
+    //funtion calling
     int ans= combination(n,r);
     cout<<"combination is "<<ans<<endl;
     return 0;
